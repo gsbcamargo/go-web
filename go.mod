4af0ce55fa708770e4bb744eed89b827190bd39f
Gabriel Schreiner <gsbcamargo@protonmail.com>
2025-05-19 21:32:35 -0300

    add modd for better devx and contact info

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gsbcamargo/go-web
+
+go 1.22.4
--- main.go
+++ main.go
@@ -5,12 +5,20 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1> Welcome to my incredibly awesome site. I mean, !</h1>")
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site. I mean, !</h1>")
+}
+
+func contactInfoHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>Contact Page</h1><p>To get in touch, e-mail me 
+	at <a href="mailto:gabriel@peticiona.ai">gabriel@peticiona.ai</a>.</p>`)
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactInfoHandler)
 	fmt.Println("Starting server on port 3000...")
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
