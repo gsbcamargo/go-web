f42c2c66d794fb599d25371950799391e515e508
Gabriel Schreiner <gsbcamargo@protonmail.com>
2025-05-24 12:25:11 -0300

    implements a load balancer of sorts (switch statement lol)

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site. I mean, !</h1>")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site. I mean, full-fledged-project-dude!</h1>")
 }
 
 func contactInfoHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,9 +16,26 @@ func contactInfoHandler(w http.ResponseWriter, r *http.Request) {
 	at <a href="mailto:gabriel@peticiona.ai">gabriel@peticiona.ai</a>.</p>`)
 }
 
+func pageNotFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>Sorry, the page you requested doesn't exist here.</h1>`)
+
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactInfoHandler(w, r)
+	default:
+		pageNotFoundHandler(w, r)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactInfoHandler)
+	http.HandleFunc("/", pathHandler)
+	// http.HandleFunc("/contact", contactInfoHandler)
 	fmt.Println("Starting server on port 3000...")
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
