56b06860fc8a9ae3a0521811c6a9a088ebb31037
Gabriel Schreiner <gsbcamargo@protonmail.com>
2025-05-27 20:54:37 -0300

    exploring http.HandlerFunc Type

--- main.go
+++ main.go
@@ -16,12 +16,6 @@ func contactInfoHandler(w http.ResponseWriter, r *http.Request) {
 	at <a href="mailto:gabriel@peticiona.ai">gabriel@peticiona.ai</a>.</p>`)
 }
 
-func pageNotFoundHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, `<h1>Sorry, the page you requested doesn't exist here.</h1>`)
-
-}
-
 func pathHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
@@ -29,16 +23,24 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 	case "/contact":
 		contactInfoHandler(w, r)
 	default:
-		pageNotFoundHandler(w, r)
+		http.Error(w, "Page not found.", http.StatusNotFound)
 	}
 }
 
+// type Router struct{}
+
+// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactInfoHandler(w, r)
+// 	default:
+// 		http.Error(w, "Page not found.", http.StatusNotFound)
+// 	}
+// }
+
 func main() {
-	http.HandleFunc("/", pathHandler)
-	// http.HandleFunc("/contact", contactInfoHandler)
 	fmt.Println("Starting server on port 3000...")
-	err := http.ListenAndServe(":3000", nil)
-	if err != nil {
-		panic(err)
-	}
+	http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
 }
